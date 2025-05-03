c2c3a566d32732cb69670a2caf7f31c2f20be0f7
Ivan <ivan.cubillos-v@mail.escuelaing.edu.co>
2025-05-02 21:54:53 -0500

    Scafolding

---
+++ src/main/java/moduloEstadisticas/Stadistics/StadisticsApplication.java
@@ -0,0 +1,13 @@
+package moduloEstadisticas.Stadistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StadisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moduloEstadisticas/Stadistics/StadisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package moduloEstadisticas.Stadistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
